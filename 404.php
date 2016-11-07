b6f3b424f5ac8af8fdbafccf4d73aa4ef0d3597d
James Cool <jeffreysbrother@gmail.com>
2016-11-07 13:18:56 -0800

    use different header/footer on 404 page

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header('front'); ?>
+
+	<div class="container">
+	  	<h1 class="main-title error-page">Sorry!</h1>
+	  	<h4>I'm having a hard time finding that page.</h4>
+	</div>
+
+<?php get_footer('front'); ?>
