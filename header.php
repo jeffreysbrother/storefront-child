cc134231c9536c2bf0f5440441a38d170717b1a8
James Cool <jeffreysbrother@gmail.com>
2016-11-07 12:49:51 -0800

    try to add custom template to storefront child

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php bloginfo('name'); ?></title>
+	<?php wp_head(); ?>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	  ga('create', 'UA-86931126-1', 'auto');
+	  ga('send', 'pageview');
+	</script>
+</head>
+<body <?php body_class(); ?>>
---
+++ template-main.php
@@ -0,0 +1,13 @@
+<?php /* Template Name: Main Template */ ?>
+<?php get_header(); ?>
+
+	<div class="container">
+	  	<h1 class="main-title">HondaRoss</h1>
+	  	<h4 class="music-title">music</h4>
+	  	<h4 class="life-title">lifestyle</h4>
+	  	<h4 class="spirit-title">spiritual arts</h4>
+	  	<h4 class="prof-title">professional advice</h4>
+	  	<h4 class="soon-title">coming soon...</h4>
+	</div>
+
+<?php get_footer(); ?>
