3dffd6c0e0a7afe3fb54e2f53f5b6c31cebd2042
James Cool <jeffreysbrother@gmail.com>
2016-11-08 13:10:49 -0800

    try to fix enqueue mess

--- functions.php
+++ functions.php
@@ -1,9 +1,24 @@
 <?php
+// function my_theme_enqueue_styles() {
+// 	// enqueue bootstrap css
+//     wp_enqueue_style( 'bootstrap_styles', get_stylesheet_directory_uri() . "/vendor/bootstrap/css/bootstrap.min.css");
+//     // enqueue child theme css
+//     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', wp_get_theme()->get('Version'));
+// }
+// add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+<?php
 function my_theme_enqueue_styles() {
-	// enqueue bootstrap css
+
+    $parent_style = 'parent-style'; 
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'bootstrap_styles', get_stylesheet_directory_uri() . "/vendor/bootstrap/css/bootstrap.min.css");
-    // enqueue child theme css
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', wp_get_theme()->get('Version'));
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
