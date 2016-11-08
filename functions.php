9bd50876906db4667dd9f3977708c3e7a7070446
James Cool <jeffreysbrother@gmail.com>
2016-11-07 21:37:05 -0800

    alternative way

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ function my_theme_enqueue_styles() {
     $parent_style = 'parent-style';
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'bootstrap_styles', get_stylesheet_directory_uri() . "/vendor/bootstrap/css/bootstrap.min.css");
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
@@ -11,12 +12,3 @@ function my_theme_enqueue_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-
-
-
-// enqueue styles
-// function honda_styles() {
-// 	wp_enqueue_style( 'bootstrap_styles', get_stylesheet_directory_uri() . "/vendor/bootstrap/css/bootstrap.min.css");
-//     wp_enqueue_style( 'main_styles', get_stylesheet_uri() . "?v=3.1.4");
-// }
-// add_action( 'wp_enqueue_scripts', 'honda_styles' );
