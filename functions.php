89d12822cbc3a5accb14545dab26d9f074593035
James Cool <jeffreysbrother@gmail.com>
2016-11-07 21:50:29 -0800

    re-ordering didn't work

--- functions.php
+++ functions.php
@@ -1,7 +1,5 @@
 <?php
 function my_theme_enqueue_styles() {
-	// enqueue parent styles
-	wp_enqueue_style('parent-theme', get_template_directory_uri() .'/style.css');
 	// enqueue bootstrap css
     wp_enqueue_style( 'bootstrap_styles', get_stylesheet_directory_uri() . "/vendor/bootstrap/css/bootstrap.min.css");
     // enqueue child theme css
