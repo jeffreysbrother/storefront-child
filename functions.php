795df2e951d83d4844eb7926878342b0a6d37fb1
James Cool <jeffreysbrother@gmail.com>
2016-11-07 22:24:42 -0800

    remove search?

--- functions.php
+++ functions.php
@@ -6,3 +6,9 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', wp_get_theme()->get('Version'));
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+
+add_action( 'wp_head', 'remove_search' );
+function remove_search(){
+	remove_action( 'storefront_header', 'storefront_product_search' );
+}
