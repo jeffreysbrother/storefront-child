48ffd3d698b109c8eee9056aec2111f20b86a9a9
James Cool <jeffreysbrother@gmail.com>
2016-11-07 16:43:57 -0800

    clean up code

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 
 	<div class="container">
 	  	<h1 class="main-title error-page">Sorry!</h1>
-	  	<h4>I'm having a hard time finding that page.</h4>
+	  	<h4 class="sub-title">I'm having a hard time finding that page.</h4>
 	</div>
 
 <?php get_footer('front'); ?>
--- template-front.php
+++ template-front.php
@@ -3,10 +3,10 @@
 
 	<div class="container">
 	  	<h1 class="main-title">HondaRoss</h1>
-	  	<h4 class="music-title">music</h4>
-	  	<h4 class="life-title">lifestyle</h4>
-	  	<h4 class="spirit-title">spiritual arts</h4>
-	  	<h4 class="prof-title">professional advice</h4>
+	  	<h4 class="sub-title">music</h4>
+	  	<h4 class="sub-title">lifestyle</h4>
+	  	<h4 class="sub-title">spiritual arts</h4>
+	  	<h4 class="sub-title">professional advice</h4>
 	  	<h4 class="soon-title">coming soon...</h4>
 	</div>
 
