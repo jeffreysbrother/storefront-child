0e575d4f9cc73b75ae307a381a52e4f417706129
James Cool <jeffreysbrother@gmail.com>
2016-11-07 21:29:54 -0800

    remove css

--- functions.php
+++ functions.php
@@ -15,8 +15,8 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 
 // enqueue styles
-function honda_styles() {
-	wp_enqueue_style( 'bootstrap_styles', get_stylesheet_directory_uri() . "/vendor/bootstrap/css/bootstrap.min.css");
-    wp_enqueue_style( 'main_styles', get_stylesheet_uri() . "?v=3.1.4");
-}
-add_action( 'wp_enqueue_scripts', 'honda_styles' );
+// function honda_styles() {
+// 	wp_enqueue_style( 'bootstrap_styles', get_stylesheet_directory_uri() . "/vendor/bootstrap/css/bootstrap.min.css");
+//     wp_enqueue_style( 'main_styles', get_stylesheet_uri() . "?v=3.1.4");
+// }
+// add_action( 'wp_enqueue_scripts', 'honda_styles' );
