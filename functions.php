5964cac8d8ba4b812bb3f0fbaf3a05f45ad2c9bd
James Cool <jeffreysbrother@gmail.com>
2016-11-08 13:15:09 -0800

    remove stuff

--- functions.php
+++ functions.php
@@ -9,15 +9,15 @@
 
 function my_theme_enqueue_styles() {
 
-    $parent_style = 'parent-style'; 
+    // $parent_style = 'parent-style'; 
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    // wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'bootstrap_styles', get_stylesheet_directory_uri() . "/vendor/bootstrap/css/bootstrap.min.css");
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
+    // wp_enqueue_style( 'child-style',
+    //     get_stylesheet_directory_uri() . '/style.css',
+    //     array( $parent_style ),
+    //     wp_get_theme()->get('Version')
+    // );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
