8b6dfc083b9c576519d660885108b1628d0dbbf8
James Cool <jeffreysbrother@gmail.com>
2016-11-09 20:10:07 -0800

    enter for testing purposes

--- template-front.php
+++ template-front.php
@@ -8,6 +8,7 @@
 	  	<h4 class="sub-title">spiritual arts</h4>
 	  	<h4 class="sub-title">professional advice</h4>
 	  	<h4 class="sub-title soon-title">coming soon...</h4>
+	  	<h4 class="sub-title soon-title"><a href="/blog">enter for testing...</a></h4>
 	</div>
 
 <?php get_footer('front'); ?>
