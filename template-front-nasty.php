0923a352d3f9dc87e28a7de97995bfab77c7a322
James Cool <jeffreysbrother@gmail.com>
2016-11-13 16:34:58 -0800

    change container

--- template-front-nasty.php
+++ template-front-nasty.php
@@ -1,15 +1,13 @@
 <?php /* Template Name: Front Template, Nasty */ ?>
 <?php get_header('front'); ?>
 
-	<div class="container">
-		<div class="nasty-wrapper">
-		  	<h1 class="main-title text-center no-line">HondaRoss</h1>
-		  	<h4 class="sub-title text-center">music</h4>
-		  	<h4 class="sub-title text-center">lifestyle</h4>
-		  	<h4 class="sub-title text-center">spiritual arts</h4>
-		  	<h4 class="sub-title text-center">professional advice</h4>
-		  	<h4 class="sub-title text-center"><a href="/blog">Enter site</a> or <a href="/shop">buy the album</a></h4>
-	  	</div>
+	<div class="container nasty-image">
+	  	<h1 class="main-title text-center no-line">HondaRoss</h1>
+	  	<h4 class="sub-title text-center">music</h4>
+	  	<h4 class="sub-title text-center">lifestyle</h4>
+	  	<h4 class="sub-title text-center">spiritual arts</h4>
+	  	<h4 class="sub-title text-center">professional advice</h4>
+	  	<h4 class="sub-title text-center"><a href="/blog">Enter site</a> or <a href="/shop">buy the album</a></h4>
 	</div>
 
 <?php get_footer('front'); ?>
