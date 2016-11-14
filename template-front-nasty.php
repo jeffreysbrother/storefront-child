acb9fb59cbbda9e8c68fad1eb85800d7919bbdb7
James Cool <jeffreysbrother@gmail.com>
2016-11-13 16:41:08 -0800

    orange block

--- template-front-nasty.php
+++ template-front-nasty.php
@@ -2,12 +2,14 @@
 <?php get_header('front'); ?>
 
 	<div class="container nasty-image">
-	  	<h1 class="main-title text-center no-line">HondaRoss</h1>
-	  	<h4 class="sub-title text-center">music</h4>
-	  	<h4 class="sub-title text-center">lifestyle</h4>
-	  	<h4 class="sub-title text-center">spiritual arts</h4>
-	  	<h4 class="sub-title text-center">professional advice</h4>
-	  	<h4 class="sub-title text-center"><a href="/blog">Enter site</a> or <a href="/shop">buy the album</a></h4>
+		<div class="block-orange">
+		  	<h1 class="main-title text-center no-line">HondaRoss</h1>
+		  	<h4 class="sub-title text-center">music</h4>
+		  	<h4 class="sub-title text-center">lifestyle</h4>
+		  	<h4 class="sub-title text-center">spiritual arts</h4>
+		  	<h4 class="sub-title text-center">professional advice</h4>
+		  	<h4 class="sub-title text-center"><a href="/blog">Enter site</a> or <a href="/shop">buy the album</a></h4>
+	  	</div>
 	</div>
 
 <?php get_footer('front'); ?>
