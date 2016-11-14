417dbd72a058e9b2be3268c61ceecaabc453fcf3
James Cool <jeffreysbrother@gmail.com>
2016-11-13 17:07:49 -0800

    fuck up text

--- template-front-nasty.php
+++ template-front-nasty.php
@@ -9,7 +9,7 @@
 			  	<h4>lifestyle</h4>
 			  	<h4 class="text-right">spiritual arts</h4>
 			  	<h4>professional advice</h4>
-			  	<h4><a href="/blog">Enter site</a> or <a href="/shop">buy the album</a></h4>
+			  	<h4 class="extending"><a href="/blog">Enter site</a> or <a href="/shop">buy the album</a></h4>
 		  	</div>
 	  	</div>
 	</div>
