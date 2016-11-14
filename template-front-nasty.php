70efe2e50a84fe04c8e49c682eb1cc99a641108a
James Cool <jeffreysbrother@gmail.com>
2016-11-13 16:25:33 -0800

    update template

--- template-front-nasty.php
+++ template-front-nasty.php
@@ -2,12 +2,12 @@
 <?php get_header('front'); ?>
 
 	<div class="container">
-	  	<h1 class="main-title text-center">HondaRoss</h1>
+	  	<h1 class="main-title text-center no-line">HondaRoss</h1>
 	  	<h4 class="sub-title text-center">music</h4>
 	  	<h4 class="sub-title text-center">lifestyle</h4>
 	  	<h4 class="sub-title text-center">spiritual arts</h4>
 	  	<h4 class="sub-title text-center">professional advice</h4>
-	  	<h4 class="sub-title text-center soon-title">coming soon...</h4>
+	  	<h4 class="sub-title text-center"><a href="/blog">Enter site</a> or <a href="/store">buy the album</a></h4>
 	</div>
 
 <?php get_footer('front'); ?>
