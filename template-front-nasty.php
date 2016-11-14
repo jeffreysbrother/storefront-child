005194a2532d0e707e7c0452d9cd9a763972aefa
James Cool <jeffreysbrother@gmail.com>
2016-11-13 16:21:54 -0800

    add new template and lightly restyle

---
+++ template-front-nasty.php
@@ -0,0 +1,13 @@
+<?php /* Template Name: Front Template, Nasty */ ?>
+<?php get_header('front'); ?>
+
+	<div class="container">
+	  	<h1 class="main-title text-center">HondaRoss</h1>
+	  	<h4 class="sub-title text-center">music</h4>
+	  	<h4 class="sub-title text-center">lifestyle</h4>
+	  	<h4 class="sub-title text-center">spiritual arts</h4>
+	  	<h4 class="sub-title text-center">professional advice</h4>
+	  	<h4 class="sub-title text-center soon-title">coming soon...</h4>
+	</div>
+
+<?php get_footer('front'); ?>
