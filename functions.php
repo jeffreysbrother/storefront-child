fca6b86b3156e5f58e7aaf93caf4eedd9d4fe174
James Cool <jeffreysbrother@gmail.com>
2016-11-14 20:53:35 -0800

    add gulp and sass?

--- functions.php
+++ functions.php
@@ -12,6 +12,7 @@
 
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'bootstrap_styles', get_stylesheet_directory_uri() . "/vendor/bootstrap/css/bootstrap.min.css");
+    wp_enqueue_style( 'compiled_scss', get_stylesheet_directory_uri() . "/css/main.css");
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
