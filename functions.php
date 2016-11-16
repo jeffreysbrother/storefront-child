5799e0f626a7c283472c73e3220118b521856abd
James Cool <jeffreysbrother@gmail.com>
2016-11-15 21:39:37 -0800

    AGAIN

--- functions.php
+++ functions.php
@@ -9,12 +9,12 @@
 
 
 function hondaross_enqueue_styles() {
-    $parent_style = 'parent-style'; 
+    // $parent_style = 'parent-style'; 
     // wp_enqueue_style( 'bootstrap_styles', get_stylesheet_directory_uri() . "/vendor/bootstrap/css/bootstrap.min.css");
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    // wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/css/main.css',
-        array( $parent_style ),
+        array( 'storefront-style' ),
         wp_get_theme()->get('Version')
     );
 }
