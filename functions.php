d846c82e9e2a67e66451487703664544a435aa07
James Cool <jeffreysbrother@gmail.com>
2016-11-16 21:37:02 -0800

    clarify

--- functions.php
+++ functions.php
@@ -12,7 +12,8 @@ add_action( 'wp_enqueue_scripts', 'wpse_173601_enqueue_scripts' );
 
 // disabling this appears to prevent the "your order" section from floating
 // when the viewport is short and you scroll.
-// oops! this disabled the cart preview you see on hovering on the nav
+// oops! Removing this disabled the cart preview you see on hovering on the nav
+
 // function de_script() {
 //     wp_dequeue_script( 'wc-cart-fragments' );
 //     return true;
