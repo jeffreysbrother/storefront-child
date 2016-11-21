0c916951d80d15161b4683528984dca9348cc0e6
James Cool <jeffreysbrother@gmail.com>
2016-11-20 17:36:30 -0800

    print_styles

--- functions.php
+++ functions.php
@@ -33,11 +33,10 @@ function child_manage_woocommerce_styles() {
 function get_rid_of_storefront_woo_css() {
 	// wp_dequeue_style( 'storefront-woocommerce-style' );
 	// wp_deregister_style( 'storefront-woocommerce-style' );
-	// remove_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
 	// wp_dequeue_style( 'storefront-woocommerce-style', get_template_directory_uri() . '/assets/sass/woocommerce/woocommerce.css' );
 	add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
 }
-add_action( 'wp_head', 'get_rid_of_storefront_woo_css' );
+add_action( 'wp_print_styles', 'get_rid_of_storefront_woo_css' );
 
 
 
